380edabe2e3a84c40a23f6e61ecc895890c3139f
Raphael Gonçalves <raphael.itprofessional@gmail.com>
2024-12-17 17:37:24 -0300

    Include e variável server

---
+++ codigo-fonte2.25.php
@@ -0,0 +1,11 @@
+<?php
+
+    $meuValor = 'Brasil';
+
+    echo($meuValor.'<BR>'); //Resultado: Brasil
+
+    include 'codigo-fonte2.24.php';
+
+    echo($meuValor.'<BR>'); // Resultado: Itália
+
+?>
---
+++ codigo-fonte2.26.php
@@ -0,0 +1,13 @@
+<?php
+
+
+    echo($_SERVER['SERVER_ADDR'].'<BR>');
+    // Resultado: 189.21.161.32, endereço do IP do Servidor
+
+    echo($_SERVER['SERVER_NAME'].'<BR>');
+    // Resutado: nome do domínio.com.br do servidor
+
+    echo($_SERVER['HTTP_ACCEPT_ENCODING'].'<BR>');
+    //Resultado: gzip, deflate
+
+?>
